3c897220f638ee78a706f90fd9baff38670a9b08
Abdurehman <abdurehmanali611@gmail.com>
2026-08-05 15:05:33 +0300

    final

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    turbopackFileSystemCacheForDev: false,
     optimizePackageImports: [
       "lucide-react",
       "date-fns",
